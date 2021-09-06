1d5d885c845c637ca779233fa051eaa1693a7f2f
Jayakrishnan M <Jayakrishnan1012@gmail.com>
2021-09-06 18:40:22 +0530

    Added Fade to Banner

--- src/Banner.js
+++ src/Banner.js
@@ -41,6 +41,8 @@ function Banner() {
                 <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
                 
             </div>
+
+            <div className="banner__fadeBottom"></div>
         </header>
     )
 }
