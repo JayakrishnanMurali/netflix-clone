fe433edb4d9d6a960cea050b905d3bb5da3ec1ba
Jayakrishnan M <Jayakrishnan1012@gmail.com>
2021-09-07 12:25:47 +0530

    Added Trailer functionality

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
       <Nav />
       <Banner />
 
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
-      <Row title="Action Movies" fetchUrl={requests.fetchTrending} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchTrending} />
-      <Row title="Romantic Movies" fetchUrl={requests.fetchTrending} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchTrending} />
-      <Row title="Documentaries" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romantic Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
 
     </div>
   );
--- src/Banner.js
+++ src/Banner.js
@@ -9,7 +9,7 @@ function Banner() {
 
     useEffect(() => {
         async function fetchData(){
-            const request = await axios.get(requests.fetchTrending);
+            const request = await axios.get(requests.fetchNetflixOriginals);
             setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)])
             return request;
         }
