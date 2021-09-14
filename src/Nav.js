aaa20588fe0ffbae038efc29d98ccaa0fd7628f7
Jayakrishnan M <Jayakrishnan1012@gmail.com>
2021-09-14 13:35:10 +0530

    Fixed netflix logo

--- src/Nav.js
+++ src/Nav.js
@@ -1,27 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import './Nav.css'
-
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+import netflixLogo from "./Asset/NetflixIcon.png";
 
 function Nav() {
-    const [show, handleShow] = useState(false);
+  const [show, handleShow] = useState(false);
 
-    useEffect(() =>{
-        window.addEventListener("scroll", () =>{
-            if (window.scrollY > 100){
-                handleShow(true);
-            } else handleShow(false);
-        });
-        return () =>{
-             window.removeEventListener("scroll");
-        };
-    },[]);
-    return (
-        <div className={`nav ${show && "nav__black"}`}>
-            <img src="../Asset/NetflixIcon.png" alt="NETFLIX Logo" className="nav__logo" />
-      
-            <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Profile" className="nav__avatar" />
-        </div>
-    )
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img src={netflixLogo} alt="NETFLIX Logo" className="nav__logo" />
+
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Profile"
+        className="nav__avatar"
+      />
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
